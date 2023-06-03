f95bef21fef7ff7f6a48e9185bb5ae6b484e9fcb
Keenah <kvancamp@mail.com>
2023-06-03 11:03:44 -0500

    Update urls.py

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
-
+from django.urls import path
+from .views import main
 urlpatterns = [
-    path('', include('api.urls'))
+    path('', main)
 ]
