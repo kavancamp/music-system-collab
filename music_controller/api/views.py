98570cc6876776ad83b143774eea36e1ecb53571
Keenah <kvancamp@mail.com>
2023-06-06 12:48:14 -0500

    Update views.py

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
 
 # Create your views here.
-def main(request):
-    return HttpResponse('Hello')
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
