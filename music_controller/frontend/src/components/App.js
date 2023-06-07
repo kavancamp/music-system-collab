24ce596e166da44d16a11ae9d2119e866ae9080a
Keenah <kvancamp@mail.com>
2023-06-07 17:10:31 -0500

    switched to createRoot

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import HomePage from "./HomePage";
 
 export default class App extends Component {
@@ -14,5 +14,7 @@ export default class App extends Component {
     );
   }
 }
+
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+const root = createRoot(appDiv);
+root.render(<App />);
