4226622d64b71234b6df7c858cbb42710dd5ce54
Keenah <kvancamp@mail.com>
2023-06-07 16:13:17 -0500

    Create RoomJoin.js

---
+++ music_controller/frontend/src/components/RoomJoin.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RoomJoin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Join Room Page</p>;
+  }
+}
