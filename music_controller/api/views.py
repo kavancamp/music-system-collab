ace01893c886d5c82d61518e1c15374be4aa66fb
Keenah <kvancamp@mail.com>
2023-06-08 10:59:13 -0500

    Update views.py

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,12 +5,14 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
 
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
